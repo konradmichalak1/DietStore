21ba94fc6bc3bc62fa20603259532837f3b3b2c0
Konrad Michalak <konradmichalak96@gmail.com>
2019-05-30 10:03:10 +0200

    Initial commit

---
+++ basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlite:C:\Users\Konrad\Documents\yii-basic-app-2.0.17\basic\db\126841',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ basic/views/site/hello2.php
@@ -0,0 +1,6 @@
+<?php use yii\helpers\Html; ?>
+<div class="site-index">
+    <div class="col-md-12">
+        <h1><?= Html::encode($message) ?></h1>
+    </div>
+</div>
